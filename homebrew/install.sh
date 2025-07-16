1665b05b79475623f479be54e8a5f3d4d15dabce
Stephen Chen <hello@stephenc.me>
2025-07-16 10:57:51 -0400

    Remove Homebrew Bundle installer, it is now included in brew by default

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,9 +9,3 @@ echo -e "\033[1;32mUpgrading Homebrew...\033[0m"
 brew upgrade
 
 echo -e "\033[1;32mInstalling Homebrew Bundle...\033[0m"
-
-if ! brew tap | grep -q "homebrew/bundle" ; then
-  brew tap homebrew/bundle
-else
-  echo -e "\033[1mHomebrew Bundle is already installed.\033[0m"
-fi
